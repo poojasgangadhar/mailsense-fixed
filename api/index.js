b3eca940332ff86359c393ce9b9629853288405d
Meghana N J <meghanamegha096@gmail.com>
2026-05-01 09:19:36 +0530

    fix: add root package.json so Vercel installs dependencies

--- api/index.js
+++ api/index.js
@@ -1,11 +1,1 @@
-try {
-  module.exports = require('../backend/server.js');
-} catch(e) {
-  module.exports = (req, res) => {
-    res.status(500).json({ 
-      error: 'Startup crash', 
-      message: e.message, 
-      stack: e.stack 
-    });
-  };
-}
+module.exports = require('../backend/server.js');
