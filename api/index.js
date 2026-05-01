ad32ee9fc11f3e66a53f754fe2b57717efd261a1
Meghana N J <meghanamegha096@gmail.com>
2026-05-01 09:13:02 +0530

    debug: catch startup crash

--- api/index.js
+++ api/index.js
@@ -1,1 +1,11 @@
-module.exports = require('../backend/server.js');
+try {
+  module.exports = require('../backend/server.js');
+} catch(e) {
+  module.exports = (req, res) => {
+    res.status(500).json({ 
+      error: 'Startup crash', 
+      message: e.message, 
+      stack: e.stack 
+    });
+  };
+}
